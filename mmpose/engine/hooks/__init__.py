d2aaa16cec527518da9efb3734c62503d70c4758
Tau <674106399@qq.com>
2023-03-07 10:56:03 +0800

    [Feature] Add RTMPose (#1971)

--- mmpose/engine/hooks/__init__.py
+++ mmpose/engine/hooks/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .ema_hook import ExpMomentumEMA
 from .visualization_hook import PoseVisualizationHook
 from .repvgg_hook import RepVGGHook
 from .nni_prune_hook import NNIPruneHook
 
-__all__ = ['PoseVisualizationHook', 'RepVGGHook', 'NNIPruneHook']
+__all__ = ['PoseVisualizationHook', 'RepVGGHook', 'NNIPruneHook', 'ExpMomentumEMA']
