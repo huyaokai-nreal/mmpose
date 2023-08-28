ed7c29d43c6336c48d32bb3d613241d09a60948c
Mesopotamia <54797851+yzd-v@users.noreply.github.com>
2023-08-28 18:33:06 +0800

    [Feature] Add DWPose (#2643)

--- mmpose/models/losses/__init__.py
+++ mmpose/models/losses/__init__.py
@@ -3,8 +3,9 @@ from .ae_loss import AssociativeEmbeddingLoss
 from .classification_loss import (BCELoss, FocalLoss, JSDiscretLoss,
                                   KLDiscretLoss, KLDiscretLoss3D)
 from .heatmap_loss import (AdaptiveWingLoss, CombinedTargetMSELoss,
-                           FocalHeatmapLoss, JointsL2Loss, KeypointMSELoss,
-                           KeypointOHKMMSELoss)
+                           FocalHeatmapLoss, JointsL2Loss, KeypointMSELoss,KeypointOHKMMSELoss)
+from .fea_dis_loss import FeaLoss
+from .logit_dis_loss import KDLoss
 from .loss_wrappers import CombinedLoss, MultipleLossWrapper
 from .regression_loss import (BoneLoss, L1Loss, MPJPELoss, MSELoss, PinchLoss,
                               RLELoss, SemiSupervisionLoss, SmoothL1Loss,
@@ -17,5 +18,7 @@ __all__ = [
     'KLDiscretLoss', 'MultipleLossWrapper', 'JSDiscretLoss', 'CombinedLoss',
     'AssociativeEmbeddingLoss', 'SoftWeightSmoothL1Loss', 'JointsL2Loss',
     'FocalHeatmapLoss', 'CombinedTargetMSELoss', 'KLDiscretLoss3D',
-    'FocalLoss', 'PinchLoss'
+    'FocalLoss', 'PinchLoss',
+    'AssociativeEmbeddingLoss', 'SoftWeightSmoothL1Loss',
+    'MPJPEVelocityJointLoss', 'FeaLoss', 'KDLoss'
 ]
--- mmpose/models/pose_estimators/__init__.py
+++ mmpose/models/pose_estimators/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .bottomup import BottomupPoseEstimator
 from .pose_attr import PoseAttr
+from .dwpose_distiller import DWPoseDistiller
 from .pose_lifter import PoseLifter
 from .topdown import TopdownPoseEstimator
 from .topdown3d import TopdownPose3DEstimator
@@ -8,5 +9,7 @@ from .topdown3d_lift import TopdownPoseLiftEstimator
 
 __all__ = [
     'TopdownPoseEstimator', 'BottomupPoseEstimator', 'TopdownPose3DEstimator',
-    'PoseLifter', 'PoseAttr', 'TopdownPoseLiftEstimator'
+    'PoseLifter', 'PoseAttr', 'TopdownPoseLiftEstimator',
+    'TopdownPoseEstimator', 'BottomupPoseEstimator', 'PoseLifter',
+    'DWPoseDistiller'
 ]
