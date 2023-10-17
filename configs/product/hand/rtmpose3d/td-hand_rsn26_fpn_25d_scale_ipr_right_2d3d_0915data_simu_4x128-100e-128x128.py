a2fb9efb77147933b9f7c2ab152ff8ff6cfeee04
lizuoxin <zx_li@nreal.ai>
2023-10-17 02:24:37 +0000

    fix(config):  fix pretrain path

--- configs/product/hand/rtmpose3d/td-hand_rsn26_fpn_25d_scale_ipr_right_2d3d_0915data_simu_4x128-100e-128x128.py
+++ configs/product/hand/rtmpose3d/td-hand_rsn26_fpn_25d_scale_ipr_right_2d3d_0915data_simu_4x128-100e-128x128.py
@@ -102,8 +102,7 @@ model = dict(
     init_cfg=dict(
         type='Pretrained',
         checkpoint=
-        #'/data/AI_DATA/data_hand/model/mmpose/td-hand_res26_fpn_skpre_flow_wd_ipr_rle_weightdata_0919_4xb64-50e-128x128/epoch_50.pth'
-        'work_dirs/td-hand_rsn26_fpn_25d_scale_ipr_dh_right_2d3d_0915data_4x128-100e-128x128/epoch_50.pth'
+        '/data/AI_DATA/data_hand/model/mmpose/td-hand_rsn26_fpn_25d_scale_ipr_dh_right_2d3d_0915data_4x128-100e-128x128/epoch_50.pth'
     ),
     root_mode='optimize' if test_type == '3d' else 'gt',
     camera_layout=camera_layout)
