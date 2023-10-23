141fbe5224750b7bb164455bddcb769c831878dc
ykhu <ykhu@xreal.com>
2023-10-23 07:50:30 +0000

    feat(model):add nano25D+liftnet model for cpu

--- mmpose/models/pose_estimators/__init__.py
+++ mmpose/models/pose_estimators/__init__.py
@@ -2,15 +2,14 @@
 from .bottomup import BottomupPoseEstimator
 from .pose_attr import PoseAttr
 from .dwpose_distiller import DWPoseDistiller
-from .dwpose_distiller3d import DWPoseDistiller3D
 from .pose_lifter import PoseLifter
-from .topdown import TopdownPoseEstimator
+from .topdown import TopdownPoseEstimator, TopdownPoseLiftEstimatorNano
 from .topdown3d import TopdownPose3DEstimator
 from .topdown3d_lift import TopdownPoseLiftEstimator
 
 __all__ = [
     'TopdownPoseEstimator', 'BottomupPoseEstimator', 'TopdownPose3DEstimator',
-    'PoseLifter', 'PoseAttr', 'TopdownPoseLiftEstimator',
+    'PoseLifter', 'PoseAttr', 'TopdownPoseLiftEstimator', 'TopdownPoseLiftEstimatorNano',
     'TopdownPoseEstimator', 'BottomupPoseEstimator', 'PoseLifter',
-    'DWPoseDistiller', 'DWPoseDistiller3D'
+    'DWPoseDistiller', 
 ]
--- mmpose/models/pose_estimators/topdown.py
+++ mmpose/models/pose_estimators/topdown.py
@@ -142,9 +142,8 @@ class TopdownPoseEstimator(BasePoseEstimator):
             feats = [_feats, _feats_flip]
         else:
             feats = self.extract_feat(inputs)
-
         preds = self.head.predict(feats, data_samples, test_cfg=self.test_cfg)
-
+        # import ipdb;ipdb.set_trace()
         if isinstance(preds, tuple):
             batch_pred_instances, batch_pred_fields = preds
         else:
@@ -153,7 +152,7 @@ class TopdownPoseEstimator(BasePoseEstimator):
 
         results = self.add_pred_to_datasample(batch_pred_instances,
                                               batch_pred_fields, data_samples)
-
+        
         return results
 
     def add_pred_to_datasample(self, batch_pred_instances: InstanceList,
@@ -217,4 +216,217 @@ class TopdownPoseEstimator(BasePoseEstimator):
                                               key)
                 data_sample.pred_fields = pred_fields
 
+        return batch_data_samples
+
+import torch
+import numpy as np
+from mmengine.structures import InstanceData
+from mmpose.models.utils import check_and_update_config
+@MODELS.register_module()
+class TopdownPoseLiftEstimatorNano(BasePoseEstimator):
+    """Base class for top-down pose estimators.
+
+    Args:
+        backbone (dict): The backbone config
+        neck (dict, optional): The neck config. Defaults to ``None``
+        head (dict, optional): The head config. Defaults to ``None``
+        train_cfg (dict, optional): The runtime config for training process.
+            Defaults to ``None``
+        test_cfg (dict, optional): The runtime config for testing process.
+            Defaults to ``None``
+        data_preprocessor (dict, optional): The data preprocessing config to
+            build the instance of :class:`BaseDataPreprocessor`. Defaults to
+            ``None``
+        init_cfg (dict, optional): The config to control the initialization.
+            Defaults to ``None``
+        metainfo (dict): Meta information for dataset, such as keypoints
+            definition and properties. If set, the metainfo of the input data
+            batch will be overridden. For more details, please refer to
+            https://mmpose.readthedocs.io/en/latest/user_guides/
+            prepare_datasets.html#create-a-custom-dataset-info-
+            config-file-for-the-dataset. Defaults to ``None``
+    """
+
+    def __init__(self,
+                 backbone: ConfigType,
+                 neck: OptConfigType = None,
+                 head: OptConfigType = None,
+                 kpt3d_lift: OptConfigType = None,
+                 train_cfg: OptConfigType = None,
+                 test_cfg: OptConfigType = None,
+                 data_preprocessor: OptConfigType = None,
+                 init_cfg: OptMultiConfig = None,
+                 kpt2d_with_depth: bool = False,
+                 kpt3d_lift_model_path: str = '',
+                 distill_model_path: str = '',
+                 with_hand_scale:bool = False):
+        super().__init__(
+            backbone,
+            neck,
+            head,
+            train_cfg,
+            test_cfg,
+            data_preprocessor,
+            init_cfg=init_cfg)
+        # Register the hook to automatically convert old version state dicts
+        self._register_load_state_dict_pre_hook(self._load_state_dict_pre_hook)
+        self.kpt2d_with_depth = kpt2d_with_depth
+        if neck is not None:
+            self.neck = MODELS.build(neck)
+        self.with_hand_scale = with_hand_scale
+        neck, head = check_and_update_config(neck, head)
+        neck, kpt3d_lift = check_and_update_config(neck, kpt3d_lift)
+        self.distill_model_path= distill_model_path
+
+        if (head is not None) and (kpt3d_lift is not None):
+            self.head = MODELS.build(head)  # adapt 2d kpts model
+            self.kpt3d_lift = MODELS.build(kpt3d_lift)
+            pretrained_dict = torch.load(kpt3d_lift_model_path)
+            liftnet_dict = {k.replace('kpt3d_lift.', ''): v for k, v in pretrained_dict['state_dict'].items() if k.startswith("kpt3d_lift.")}
+            self.kpt3d_lift.load_state_dict(liftnet_dict) 
+
+    def extract_feat(self, inputs: Tensor) -> Tuple[Tensor]:
+        """Extract features.
+
+        Args:
+            inputs (Tensor): Image tensor with shape (N, C, H ,W).
+
+        Returns:
+            tuple[Tensor]: Multi-level features that may have various
+            resolutions.
+        """
+        x = self.backbone(inputs)
+        if self.with_neck:
+            x = self.neck(x)
+        return x
+
+    def _forward(self, inputs: Tensor):
+        """Network forward process. Usually includes backbone, neck and head
+        forward without any post-processing.
+
+        Args:
+            inputs (Tensor): Inputs with shape (N, C, H, W).
+
+        Returns:
+            tuple: A tuple of features from ``rpn_head`` and ``roi_head``
+            forward.
+        """
+
+        x = self.extract_feat(inputs)
+        if self.with_head:
+            x = self.head.forward(x)
+            if isinstance(x, list):
+                x = x[-1]
+        return x
+
+    @format_data
+    def loss(self, inputs: Tensor, data_samples: SampleList) -> dict:
+        """Calculate losses from a batch of inputs and data samples.
+
+        Args:
+            inputs (Tensor): Inputs with shape (N, C, H, W).
+            data_samples (List[:obj:`PoseDataSample`]): The batch
+                data samples.
+
+        Returns:
+            dict: A dictionary of losses.
+        """
+        with torch.no_grad():
+            feats_pyramid = self.extract_feat(inputs)
+            outputs = self.head.forward(feats_pyramid)
+            xy_sigma = outputs[:2]  # 2d
+            if self.kpt2d_with_depth:
+                depth = outputs[2]
+                xy_sigma = torch.cat([xy_sigma, depth], dim=-1)
+        losses = self.kpt3d_lift.loss(xy_sigma, data_samples)
+        return losses
+
+    @format_data
+    def predict(self, inputs: Tensor, data_samples: SampleList) -> SampleList:
+        """Predict results from a batch of inputs and data samples with post-
+        processing.
+
+        Args:
+            inputs (Tensor): Inputs with shape (N, C, H, W)
+            data_samples (List[:obj:`PoseDataSample`]): The batch
+                data samples
+
+        Returns:
+            list[:obj:`PoseDataSample`]: The pose estimation results of the
+            input images. The return value is `PoseDataSample` instances with
+            ``pred_instances`` and ``pred_fields``(optional) field , and
+            ``pred_instances`` usually contains the following keys:
+
+                - keypoints (Tensor): predicted keypoint coordinates in shape
+                    (num_instances, K, D) where K is the keypoint number and D
+                    is the keypoint dimension
+                - keypoint_scores (Tensor): predicted keypoint scores in shape
+                    (num_instances, K)
+        """
+        
+        if self.distill_model_path:
+            distill_model = torch.load(self.distill_model_path)
+            state_dict = {k.replace('student.', ''): v for k, v in distill_model['state_dict'].items() if not k.startswith('teacher')}
+            backbone_state_dict = {k.replace('backbone.', ''): v for k, v in state_dict.items() if k.startswith('backbone')}
+            head_state_dict = {k.replace('head.', ''): v for k, v in state_dict.items() if k.startswith('head')}
+            self.backbone.load_state_dict(backbone_state_dict)
+            self.head.load_state_dict(head_state_dict)
+        assert self.with_head, (
+            'The model must have head to perform prediction.')
+
+        if self.test_cfg.get('flip_test', False):
+            _feats = self.extract_feat(inputs)
+            _feats_flip = self.extract_feat(inputs.flip(-1))
+            feats = [_feats, _feats_flip]
+        else:
+            feats = self.extract_feat(inputs)
+        preds = self.head.predict(feats, data_samples, test_cfg=self.test_cfg)
+        
+        xy_sigma = []
+        for i in range(len(preds)):
+            xy_sigma.append(preds[i].keypoints)
+        xy_sigma = np.concatenate(xy_sigma,axis=0)
+        pred, pred_bino_kp2d = self.kpt3d_lift.predict(
+            xy_sigma, data_samples, test_cfg=self.test_cfg)
+        
+        batch_pred_instances = []
+        for b in range(pred.shape[0]):
+            keypoints = pred_bino_kp2d[b:b + 1, 0, ...]  # 左目信息
+            batch_pred_instances.append(
+                InstanceData(
+                    keypoints3d=pred[b:b + 1, ...],
+                    keypoints3d_scores=torch.ones((1, 21)),
+                    keypoints=keypoints,
+                    keypoint_scores=torch.ones((1, 21)),
+                ))
+
+        results = self.add_pred_to_datasample(batch_pred_instances, None,
+                                              data_samples)
+        return results
+
+    def add_pred_to_datasample(self, batch_pred_instances: InstanceList,
+                               batch_pred_fields: Optional[PixelDataList],
+                               batch_data_samples: SampleList) -> SampleList:
+
+        batch_data_samples = batch_data_samples[::2]  # 3d gt信息保存在左目
+
+        assert len(batch_pred_instances) == len(batch_data_samples)
+        if batch_pred_fields is None:
+            batch_pred_fields = []
+
+        for pred_instances, pred_fields, data_sample in zip_longest(
+                batch_pred_instances, batch_pred_fields, batch_data_samples):
+            if data_sample.meta['flipped']:
+                data_sample.gt_instances.keypoints3d[...,0] *= -1
+            pred_instances.keypoints3d = pred_instances.keypoints3d.cpu(
+            ).numpy()
+            pred_instances.keypoints3d_scores = np.ones(
+                (1, pred_instances.keypoints3d.shape[1]))
+            pred_instances.keypoints = pred_instances.keypoints.cpu().numpy()
+            pred_instances.keypoints = np.concatenate((pred_instances.keypoints, pred_instances.keypoints3d[...,2:]), axis=-1)
+            data_sample.gt_instances.keypoints = np.concatenate((data_sample.gt_instances.keypoints[...,:2], data_sample.gt_instances.keypoints3d[...,2:]), axis=-1)
+            pred_instances.keypoint_scores = np.ones(
+                (1, pred_instances.keypoints.shape[1]))
+
+            data_sample.pred_instances = pred_instances
         return batch_data_samples
