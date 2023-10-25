7ed81644b97c7f6fd8d9ea961290bc9df1343a17
ykhu <ykhu@xreal.com>
2023-10-25 08:39:11 +0000

    fix(metric):add keypoints to MPJPEV2

--- mmpose/evaluation/metrics/keypoint_3d_metrics.py
+++ mmpose/evaluation/metrics/keypoint_3d_metrics.py
@@ -151,7 +151,7 @@ class MPJPEV2(MPJPE):
                  pinch_thre: list = [20, 40],
                  scale_metric: bool = False,
                  bmk_save_root: str = '',
-                 show_bmk_thr:float = 20,
+                 show_bmk_thr:tuple = (0,10),
                  filter_exceed:bool = True) -> None:
         super().__init__(collect_device=collect_device, prefix=prefix)
         self.result_dir = result_dir
@@ -182,6 +182,18 @@ class MPJPEV2(MPJPE):
                 (-1)).tolist()
             result.score = float(np.mean(keypoint_scores))
             result.video_id = data_sample['img_path']
+            import ipdb;ipdb.set_trace()
+            if data_sample['meta']['flipped']:
+                pred_kpt = data_sample['pred_instances']['keypoints'][0]
+                gt_kpt = gt['keypoints'][0]
+                pred_kpt[...,0] = (data_sample['meta']['frame_width'] - 1 - pred_kpt[...,0])
+                gt_kpt[...,0] = (data_sample['meta']['frame_width'] - 1 - gt_kpt[...,0])
+                result.keypoints = pred_kpt.tolist()
+                result.gt_keypoints = gt_kpt.tolist()
+            else:
+                result.keypoints = (
+                    data_sample['pred_instances']['keypoints'][0]).tolist()
+                result.gt_keypoints = gt['keypoints'][0].tolist()
             if 'tag' in data_sample['meta']:
                 result.meta['tag'] = data_sample['meta']['tag']
             if 'gesture' in data_sample['meta']:
