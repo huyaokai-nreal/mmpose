d85434aac45d33c42aedeaa7554e4eed0d41d09d
lizuoxin <zx_li@nreal.ai>
2024-02-02 08:54:08 +0000

    feat(data): add flora train data

--- mmpose/configs/_base_/datasets/xs2d.py
+++ mmpose/configs/_base_/datasets/xs2d.py
@@ -35,7 +35,8 @@ datasets_info = {
             'data_hand/hand_keypoint/annotations/hand_train_flora_20k_230829_1_cam0_lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_20k__230914__1__cam0__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_keypoint_231027_20k__1__binocular__lmdb.json',
-            'data_hand/hand_keypoint/annotations/hand_train_flora_keypoint_decoration_1_231017_20k__1__binocular__lmdb.json'
+            'data_hand/hand_keypoint/annotations/hand_train_flora_keypoint_decoration_1_231017_20k__1__binocular__lmdb.json',
+            'data_hand/hand_keypoint/annotations/hand_train_flora_keypoint_bottom_1_231017_20k__1__binocular__lmdb.json'
         ]
     },
     'test_data': {
