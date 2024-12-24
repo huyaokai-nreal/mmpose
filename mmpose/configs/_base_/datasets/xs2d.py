caa17145954ed47d34afdb41309b991976d8ba32
ykhu <ykhu@xreal.com>
2024-12-24 09:14:13 +0000

    feat(data):add 2d data 7.6k

--- mmpose/configs/_base_/datasets/xs2d.py
+++ mmpose/configs/_base_/datasets/xs2d.py
@@ -71,6 +71,8 @@ datasets_info = {
             'data_hand/hand_keypoint/annotations/hand_train_flora_e2e_20241220_6__1__binocular__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_e2e_20241220_7__1__binocular__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_e2e_20241220_8__1__binocular__lmdb.json',
+            'data_hand/hand_keypoint/annotations/hand_train_flora_20241222__1__binocular__lmdb.json',
+            'data_hand/hand_keypoint/annotations/hand_train_flora_20241223__1__binocular__lmdb.json',
         ]
     },
     'test_data': {
