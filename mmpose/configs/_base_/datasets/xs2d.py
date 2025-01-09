23ff833e1128650731a28a4df269f6eac5720559
ykhu <ykhu@xreal.com>
2025-01-09 06:38:09 +0000

    feat(data):add 2d data

--- mmpose/configs/_base_/datasets/xs2d.py
+++ mmpose/configs/_base_/datasets/xs2d.py
@@ -72,6 +72,8 @@ datasets_info = {
             'data_hand/hand_keypoint/annotations/hand_train_flora_e2e_20241220_6__1__binocular__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_e2e_20241220_7__1__binocular__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_e2e_20241220_8__1__binocular__lmdb.json',
+            'data_hand/hand_keypoint/annotations/hand_train_flora_20241123__1__binocular__lmdb.json',
+            'data_hand/hand_keypoint/annotations/hand_train_flora_20241129__1__binocular__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_20241222__1__binocular__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_20241223__1__binocular__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_20241226__1__binocular__lmdb.json',
@@ -79,6 +81,7 @@ datasets_info = {
             'data_hand/hand_keypoint/annotations/hand_train_flora_20250102__1__binocular__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_20250103__1__binocular__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_20250104__1__binocular__lmdb.json',
+            'data_hand/hand_keypoint/annotations/hand_train_flora_20250106__1__binocular__lmdb.json',
         ]
     },
     'test_data': {
