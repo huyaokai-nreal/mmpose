923db58094143e401a7ea0c101dfbce202c83ee1
ykhu <ykhu@xreal.com>
2025-01-17 09:55:38 +0000

    feat(data):add 3.6k 2d data

--- mmpose/configs/_base_/datasets/xs2d.py
+++ mmpose/configs/_base_/datasets/xs2d.py
@@ -82,6 +82,7 @@ datasets_info = {
             'data_hand/hand_keypoint/annotations/hand_train_flora_20250103__1__binocular__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_20250104__1__binocular__lmdb.json',
             'data_hand/hand_keypoint/annotations/hand_train_flora_20250106__1__binocular__lmdb.json',
+            'data_hand/hand_keypoint/annotations/hand_train_flora_20250115__1__binocular__lmdb.json',
         ]
     },
     'test_data': {
